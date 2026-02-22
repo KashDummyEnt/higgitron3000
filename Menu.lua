64c6f4c6a7d34fe3a74dd619864ca291ddcbbc37
KashDummyEnt <kashdummybooking@gmail.com>
2026-02-22 12:06:13 -0600

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -20,7 +20,7 @@ local CONFIG = {
 
 	PopupSize = Vector2.new(600, 400),
 
-	BaseAccent = Color3.fromRGB(255, 0, 0),
+	BaseAccent = Color3.fromRGB(255, 0, 255),
 	Accent = Color3.fromRGB(255, 0, 0),
 
 	Bg = Color3.fromRGB(14, 14, 16),
